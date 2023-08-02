da03e02c5492092d7bc8c4e31df04b95868fceef
vpepe <valerio.pepe@gmail.com>
2023-08-02 17:48:14 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-mkdir -p $PREFIX/bin
-
-chmod SeqVerify-prerelease/seqverify +x
-cp SeqVerify-prerelease/seqverify $PREFIX/bin
-cp SeqVerify-prerelease/seqver_functions.py $PREFIX/bin
-cp SeqVerify-prerelease/seqver_transgenes.py $PREFIX/bin
