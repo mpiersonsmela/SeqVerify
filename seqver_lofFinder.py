99624f8ea4ed6214667b70ad7412db2917519382
vpepe <valerio.pepe@gmail.com>
2024-06-17 13:03:09 -0400

    Updated variant calling pipeline

--- seqver_lofFinder.py
+++ seqver_lofFinder.py
@@ -1,7 +1,20 @@
 import matplotlib.pyplot as plt
 
-def mutation_logger(folder,output_file,VCF_file, min_quality, min_intensity="MODERATE"):
+def mutation_logger(folder,output_file,VCF_file, min_quality, min_intensity="MODERATE", window_size = 10000, command_file=None):
     qual_scores = []
+
+    command_coordinates = {}
+    if command_file is not None:
+        with open(command_file,"r") as commands:
+            for command in commands:
+                coords = command.split("\t")[0].split(":") #command coordinates come in a CHR:START-END format, so this isolates the two major components
+                chromosome = coords[0] #selects the CHR part
+                position = int(coords[1].split("-")[0]) #takes START-END, selects START
+                try:
+                    command_coordinates[chromosome].append(position)
+                except KeyError:
+                    command_coordinates[chromosome] = [position]
+
     with open(f"{folder}/{VCF_file}","r") as vcf:
         intensities = ["MODIFIER","LOW","MODERATE","HIGH"]
         min_intensity_index = intensities.index(min_intensity)
@@ -13,7 +26,16 @@ def mutation_logger(folder,output_file,VCF_file, min_quality, min_intensity="MOD
                     coords,quality,annotations,genotype = ":".join(fields[0:2]),fields[5],fields[7].split(";"),fields[-1].split(":")[0]
                     quality = float(quality)
                     qual_scores.append(quality)
-                    if quality >= float(min_quality):
+
+                    close_to_command = False
+                    if fields[0] in command_coordinates.keys(): 
+                        #If no command file is specified, command_coordinates.keys() will be an empty set so this condition will never trigger.
+                        #fields[0] is the chromosome of the variant, fields[1] is its coordinate
+                        differences = [abs(int(position) - int(fields[1])) <= window_size for position in command_coordinates[fields[0]]]
+                        if any(differences):
+                            close_to_command = True
+
+                    if quality >= float(min_quality) or close_to_command:
                         homozygous = (len(set(genotype.split("/"))) == 1)
                         
                         relevant = [i for i in annotations if i.startswith("ANN") or i.startswith("LOF")]
@@ -24,10 +46,11 @@ def mutation_logger(folder,output_file,VCF_file, min_quality, min_intensity="MOD
                         except:
                             LOF_gene = False
 
+
                         for ann in annotations:
                             args = ann.split("|")
 
-                            if args[2] not in intensities[0:min_intensity_index]:
+                            if args[2] not in intensities[0:min_intensity_index] or close_to_command:
                                 id_str = coords,str(quality),args[1],args[2],args[3],args[6],args[9], args[10], homozygous,LOF_gene
                                 id_str = [str(i) for i in id_str]
                                 output.write("\t".join(id_str)+"\n")
